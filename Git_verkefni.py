5f0ded267cbbc75da55ca05e7899179d3368144b
Hákon Dagur Oddgeirsson <hakonod307@nemi.tskoli.is>
2017-01-25 10:59:49 +0000

    verkefni 1

---
+++ Git_verkefni.py
@@ -0,0 +1,11 @@
+#Hákon Dagur Oddgeirsson
+#25.01.2017
+#git_verkefni
+
+print("Sláðu inn 2 tölur")
+tala1=int(input("Sláðu inn fyrstu töluna:"))
+tala2=int(input("Sláðu inn seinni töluna:"))
+plus=tala1 + tala2
+sinnum=tala1 * tala2
+print("Ef þú plúsaðar tölurnar saman færðu útkommuna", plus)
+print("Ef þú margfaldar tölurnar saman færðu útkommuna", sinnum)
