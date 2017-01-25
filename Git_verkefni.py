33b50220dda1b35911a669738908d61bbaa7f17f
Hákon Dagur Oddgeirsson <hakonod307@nemi.tskoli.is>
2017-01-25 11:03:53 +0000

    verkefni 2

--- Git_verkefni.py
+++ Git_verkefni.py
@@ -9,3 +9,10 @@ plus=tala1 + tala2
 sinnum=tala1 * tala2
 print("Ef þú plúsaðar tölurnar saman færðu útkommuna", plus)
 print("Ef þú margfaldar tölurnar saman færðu útkommuna", sinnum)
+
+#dæmi2
+
+print("Sláðu inn nafnið þitt")
+fornafn=input("Sláðu inn fornafn þitt hér:")
+eftirnafn=input("Sláðu inn eftirnafn þitt hér:")
+print("Halló" , fornafn , eftirnafn)
