42e1170b718026f4e35a037ead70d70e04c1eeec
Hákon Dagur Oddgeirsson <hakonod307@nemi.tskoli.is>
2017-01-25 12:15:06 +0000

    verkefni 3

--- Git_verkefni.py
+++ Git_verkefni.py
@@ -16,3 +16,30 @@ print("Sláðu inn nafnið þitt")
 fornafn=input("Sláðu inn fornafn þitt hér:")
 eftirnafn=input("Sláðu inn eftirnafn þitt hér:")
 print("Halló" , fornafn , eftirnafn)
+
+#Dæmi 3
+
+print()
+lagstaf = 0
+hastaf = 0
+eftirha = 0
+minus = -1
+text = input("Sláðu inn texta: ")
+leng = int(len(text))
+listi = list(text)
+
+for x in listi:
+    if x.isupper():
+        hastaf += 1
+    elif x.islower():
+        lagstaf += 1
+
+for x in range(leng):
+    if listi[x].islower() and listi[minus].isupper():
+        minus += 1
+        eftirha += 1
+    else:
+        minus += 1
+
+print("Í þessum texta eru",hastaf,"hástafir,",lagstaf,"lágstafir og",eftirha,
+      "lágstafir strax á eftir hástaf")
